2e4159f8ddcf8838762a33e445ea4465b01a131b
Majed Abouhatab, P.E <45211996+MajedAbouhatab@users.noreply.github.com>
2021-09-12 19:18:31 -0500

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -2,8 +2,8 @@
 
 #define THINGNAME String("ESP_" + String(ESP_getChipId())).c_str()
 #define UpdateShadow "$aws/things/" + String(THINGNAME) + "/shadow/update"
-String StageURL = ""
-const char AWS_IOT_ENDPOINT[] = ""
+String StageURL = "";
+const char AWS_IOT_ENDPOINT[] = "";
 
 // Amazon Root CA 1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
